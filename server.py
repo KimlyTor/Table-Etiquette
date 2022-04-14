e95f8f7bf192702caaf191c1ea8eab074539f47e
Seung J Shin <seungj@MacBook-Pro-10.local>
2022-04-14 15:46:30 -0400

    Completed skeletal pages of the project.

--- server.py
+++ server.py
@@ -10,6 +10,20 @@ app = Flask(__name__)
 def welcome():
     return render_template('home.html')
 
+
+@app.route('/basic_rules')
+def basic_rules():
+    return render_template('basic_rules.html')
+
+
+@app.route('/table_setting')
+def table_setting():
+    return render_template('table_setting.html')
+
+
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')
 # AJAX FUNCTIONS
 
 
