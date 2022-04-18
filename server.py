78c9c39e2987577062a8b0a8a9a692851a413e49
ktkay <kt2593@columbia.edu>
2022-04-18 09:24:20 -0400

    set up homepage, make the images show up, centerd the text

--- server.py
+++ server.py
@@ -3,9 +3,9 @@ from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
+
+
 # ROUTES
-
-
 @app.route('/')
 def welcome():
     return render_template('home.html')
